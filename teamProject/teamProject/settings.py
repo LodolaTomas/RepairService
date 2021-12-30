81da700ed3f0f3face16ba240f4b15c5c5d25fd7
Elian <elifbaque@gmail.com>
2021-12-30 05:32:15 -0300

    Cors Settings

--- teamProject/teamProject/settings.py
+++ teamProject/teamProject/settings.py
@@ -42,13 +42,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
     'rest_framework',
+    'corsheaders',
+    # Our Apps
     'serviceRepairBackEnd',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
