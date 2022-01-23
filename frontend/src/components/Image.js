e950185c00546c06326f76abeefb05c6033775c0
LodolaTomas <tomaslodola1@gmail.com>
2022-01-23 03:51:35 -0300

    commitiando por commitiar

--- frontend/src/components/Image.js
+++ frontend/src/components/Image.js
@@ -1,5 +1,7 @@
 export const Image = ({image}) => {
     return (
-        <img width='100' height="100" src={'http://127.0.0.1:8000'+ image} alt="" />
+        <picture>
+            <img width='100' height="100" src={'http://127.0.0.1:8000'+ image} alt="" />
+        </picture>
     );
 }
