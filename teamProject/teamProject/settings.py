77319597472d4bf93b5dd245877b3d5247b638c5
Elian Baque <elifbaque@gmail.com>
2022-03-16 23:51:39 -0300

    Update settings.py

--- teamProject/teamProject/settings.py
+++ teamProject/teamProject/settings.py
@@ -104,12 +104,6 @@ DATABASES = {
 	}
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'elifbaqueprivado@gmail.com'
-EMAIL_HOST_PASSWORD = 'ngvyjopdlexejmzi'
-EMAIL_USE_TLS = True
 
 
 # Password validation
